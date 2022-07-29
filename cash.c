671ed959b00769c99c75933691a3de1b8ab67565
Anshul Singh <anshulspidy@gmail.com>
2022-07-29 18:42:07 +0530

    Added built-in commands

--- cash.c
+++ cash.c
@@ -75,6 +75,87 @@ char **cash_split_line(char *line)
 }
 
 
+/*
+  Function Declarations for builtin shell commands:
+ */
+int cash_cd(char **args);
+int cash_help(char **args);
+int cash_exit(char **args);
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &cash_cd,
+  &cash_help,
+  &cash_exit
+};
+
+int cash_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+  Builtin function implementations.
+*/
+int cash_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "cash: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("cash");
+    }
+  }
+  return 1;
+}
+
+int cash_help(char **args)
+{
+  int i;
+  printf("cash-shell help:\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < cash_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int cash_exit(char **args)
+{
+  return 0;
+}
+
+
+int cash_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < cash_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return cash_launch(args);
+}
+
+
 void cash_loop() {
     char *line;
     char **args;
